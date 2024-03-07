45d3bf9b5d99116aaba13630cb127130e62ee9fd
DiogoBastelli <90928308+DiogoBastelli@users.noreply.github.com>
2024-03-07 16:19:28 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const axios = require('axios')
-var nome = document.getElementById("nome").value;
-const url = `https://api.github.com/users/${nome}/repos`
-
-
-async function getRepos(){
-     axios
-     .get(url)
-    .then(response =>{
-        console.log(response.data)
-        console.log('sucesso')
-    })
-    .catch(e =>{
-        console.log(e)
-        console.log('Error 404')
-    })
-    .finally(f =>{
-        console.log('finalizou a request')
-    })
+async function getRepos() {
+    try {
+        const username = document.getElementById('usuario').value;
+        const url = `https://api.github.com/users/${username}/repos`;
+        const response = await axios.get(url);
+        
+        const repos = response.data;
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.innerHTML = ''; // Limpar o conteúdo anterior
+        if (repos.length > 0) {
+            const reposList = document.createElement('ul');
+            repos.forEach(repo => {
+                const listItem = document.createElement('li');
+                listItem.textContent = repo.name;
+                reposList.appendChild(listItem);
+            });
+            resultadoDiv.appendChild(reposList);
+        } else {
+            resultadoDiv.textContent = 'Nenhum repositório encontrado.';
+        }
+        console.log('Sucesso');
+    } catch (error) {
+        console.error('Erro ao obter repositórios:', error);
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.textContent = 'Ocorreu um erro ao buscar os repositórios. Verifique se o nome de usuário está correto.';
+    } finally {
+        console.log(username);
+    }
 }
-
-//getRepos()
